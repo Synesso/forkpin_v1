10db237439b42b9d94ca617823ebaa28647a1d98
Jem Mawson <jem.mawson@gmail.com>
2013-08-30 16:25:15 +0800

    Serialisation spec done

--- src/test/scala/forkpin/SerialisationSpec.scala
+++ src/test/scala/forkpin/SerialisationSpec.scala
@@ -11,14 +11,19 @@ import org.specs2.execute.Result
 class SerialisationSpec extends Specification with ScalaCheck with Mockito { def is = s2"""
 
   A list of moves should
-    have $lengthMultipleOf4
-
+    have length being a multiple of 4 $lengthMultipleOf4
+    every odd character must be a valid file $oddCharsAreFiles
+    every even character must be a valid rank $evenCharsAreRanks
 
 """
 
-  // todo - more work required
-
   def lengthMultipleOf4 = pickledArbitraryMoves(_.length % 4 must beEqualTo(0))
+
+  def oddCharsAreFiles = pickledArbitraryMoves(_.grouped(2).foldLeft("")
+    {(acc,next) => acc + next.head}.forall{c => c must (beGreaterThanOrEqualTo('A') and beLessThanOrEqualTo('H'))})
+
+  def evenCharsAreRanks = pickledArbitraryMoves(_.grouped(2).foldLeft("")
+    {(acc,next) => acc + next.tail.head}.forall{c => c must (beGreaterThanOrEqualTo('1') and beLessThanOrEqualTo('8'))})
 
   val moves = (for {from <- RankAndFile.values; to <- RankAndFile.values} yield Move(from, to)).toSeq
   implicit val arbitraryMoveList = Arbitrary(listOf(Gen.oneOf(moves)))
