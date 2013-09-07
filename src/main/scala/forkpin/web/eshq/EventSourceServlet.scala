829db321c2554096cda46b7cdb64b97bee781ac3
Jem Mawson <jem.mawson@gmail.com>
2013-09-07 22:10:04 +0800

    Moved ESHQ to external jar

--- src/main/scala/forkpin/web/eshq/EventSourceServlet.scala
+++ src/main/scala/forkpin/web/eshq/EventSourceServlet.scala
@@ -1,16 +1,24 @@
 package forkpin.web.eshq
 
-import EventSource._
 import forkpin.web.ForkpinServlet
+import com.github.synesso.eshq.{Channel, Key, Secret, EventSourceClient}
+import forkpin.Config
+import java.net.URL
+import org.scalatra.FutureSupport
+import scala.concurrent.ExecutionContext
 
-class EventSourceServlet extends ForkpinServlet {
+class EventSourceServlet extends ForkpinServlet with Config with FutureSupport {
+  val executor: ExecutionContext = ExecutionContext.global
+
+  private val (key, secret, serviceURL) = (properties("ESHQ_KEY"), properties("ESHQ_SECRET"), properties("ESHQ_URL"))
+  private val client = EventSourceClient(Key(key), Secret(secret), new URL(serviceURL))
 
   post("/socket") {
-    createSocket(params("channel"))
+    client.open(Channel(params("channel")))
   }
 
   get("/test") {
-    publish("forkpin", "all-done!")
+    client.send(Channel("forkpin"), "make it funky da da da")
   }
 
 }
