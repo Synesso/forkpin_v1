59b940f2f5ba3a0ed5f0ca61423f84a1e4e7efbf
Jem Mawson <jem.mawson@gmail.com>
2013-09-14 15:29:42 +0800

    move serialisation spec updated with richer Move arbitrary instances

--- src/test/scala/forkpin/SerialisationSpec.scala
+++ src/test/scala/forkpin/SerialisationSpec.scala
@@ -1,23 +1,22 @@
 package forkpin
 
 import org.specs2.{ScalaCheck, Specification}
-import org.scalacheck.{Gen, Arbitrary}
-import org.scalacheck.Gen._
-import org.specs2.mock.Mockito
-import forkpin.persist.Persistent.User
 import org.specs2.execute.Result
+import RankAndFile._
 
 
-class SerialisationSpec extends Specification with ScalaCheck with Mockito { def is = s2"""
+class SerialisationSpec extends Specification with ScalaCheck with TestImplicits { def is = s2"""
 
-  A list of moves should
-    have length being a multiple of 4 $lengthMultipleOf4
+  A serialised list of moves should
+    have length exactly 4 times the input $length4TimesInput
     every odd character must be a valid file $oddCharsAreFiles
     every even character must be a valid rank $evenCharsAreRanks
 
 """
 
-  def lengthMultipleOf4 = pickledArbitraryMoves(_.length % 4 must beEqualTo(0))
+  def length4TimesInput = prop{(moves: List[Move]) =>
+    startGame.copy(moves = moves.toVector).pickledMoves.length must beEqualTo(moves.length * 4)
+  }
 
   def oddCharsAreFiles = pickledArbitraryMoves(_.grouped(2).foldLeft("")
     {(acc,next) => acc + next.head}.forall{c => c must (beGreaterThanOrEqualTo('A') and beLessThanOrEqualTo('H'))})
@@ -25,13 +24,7 @@ class SerialisationSpec extends Specification with ScalaCheck with Mockito { def
   def evenCharsAreRanks = pickledArbitraryMoves(_.grouped(2).foldLeft("")
     {(acc,next) => acc + next.tail.head}.forall{c => c must (beGreaterThanOrEqualTo('1') and beLessThanOrEqualTo('8'))})
 
-  val moves = (for {from <- RankAndFile.values; to <- RankAndFile.values} yield Move(from, to)).toSeq
-  implicit val arbitraryMoveList = Arbitrary(listOf(Gen.oneOf(moves)))
-
   def pickledArbitraryMoves(p: (String) => Result) = prop{(moves: List[Move]) =>
-    val game = Game(1, white, black).copy(moves = moves.toVector)
-    p(game.pickledMoves)
+    p(startGame.copy(moves = moves.toVector).pickledMoves)
   }
-
-  val white, black = mock[User]
 }
--- src/test/scala/forkpin/TestImplicits.scala
+++ src/test/scala/forkpin/TestImplicits.scala
@@ -1,12 +1,15 @@
 package forkpin
 
 import forkpin.RankAndFile._
+import org.specs2.mock.Mockito
+import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Gen._
 import scala.Some
 import forkpin.persist.Persistent.User
-import org.specs2.mock.Mockito
 
 trait TestImplicits extends Mockito {
 
+  // games
   val black = mock[User]
   val white = mock[User]
   val startGame = Game(1, white, black, white)
@@ -14,17 +17,20 @@ trait TestImplicits extends Mockito {
   val castleGame = startGame.copy(board = Board(pieces = Vector[(RankAndFile, Piece)](
     A1 -> WhiteRook, E1 -> WhiteKing, H1 -> WhiteRook,
     A8 -> BlackRook, E8 -> BlackKing, H8 -> BlackRook
-  ).foldLeft(Vector.fill(64)(None: Option[Piece])){(arr, next) =>
-    arr.updated(next._1.id, Some(next._2))
+  ).foldLeft(Vector.fill(64)(None: Option[Piece])) {
+    (arr, next) =>
+      arr.updated(next._1.id, Some(next._2))
   }))
 
   def game(pieces: (RankAndFile, Piece)*): Game = {
-    val b = Board(pieces = pieces.foldLeft(Vector.fill(64)(None: Option[Piece])){(arr, next) =>
-      arr.updated(next._1.id, Some(next._2))
+    val b = Board(pieces = pieces.foldLeft(Vector.fill(64)(None: Option[Piece])) {
+      (arr, next) =>
+        arr.updated(next._1.id, Some(next._2))
     })
     Game(1, white, black, white, board = b)
   }
 
+  // extra game methods for test-time sweets
   implicit class GameWrapper(game: Game) {
     def place(rfPiece: (RankAndFile, Piece)) = {
       val (rf, piece) = rfPiece
@@ -33,15 +39,34 @@ trait TestImplicits extends Mockito {
     }
 
     def surround(rf: RankAndFile, piece: Piece) = {
-      rf.surroundingSquares.foldLeft(game){case (buildingGame, square) => buildingGame.place(square, piece)}
+      rf.surroundingSquares.foldLeft(game) {
+        case (buildingGame, square) => buildingGame.place(square, piece)
+      }
     }
 
     def printBoard(): Unit = {
-      game.board.pieces.zipWithIndex.foreach{case (op, i) =>
-        print(op.map(p => p.sanModifier(p.sanRole)).getOrElse("."))
-        if (i % 8 == 7) println()
+      game.board.pieces.zipWithIndex.foreach {
+        case (op, i) =>
+          print(op.map(p => p.sanModifier(p.sanRole)).getOrElse("."))
+          if (i % 8 == 7) println()
       }
     }
   }
+
+  // scalacheck generators
+  val rankAndFileGenerator = Gen.oneOf(RankAndFile.values.toSeq)
+  val promotionGenerator = Gen.oneOf(Seq(Promotion(A1, WhiteQueen))) // todo - better
+  def optional[T](g: Gen[T]) = for (qty <- Gen.choose(0, 1); xs <- Gen.listOfN[T](qty, g)) yield xs.headOption
+  val moveGenerator: Gen[Move] = for {
+    from <- rankAndFileGenerator
+    to <- rankAndFileGenerator
+    capture <- optional(rankAndFileGenerator)
+    implication <- optional(moveGenerator)
+    promotion <- optional(promotionGenerator)
+  } yield Move(from, to, capture, implication, promotion)
+
+  // scalacheck implicity arbitrary values
+  implicit val arbitraryMoveList = Arbitrary(listOf(moveGenerator))
+
 
 }
