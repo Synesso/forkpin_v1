707e724f4a677cdc9b424db55b76cf589240672c
Jem Mawson <jem.mawson@gmail.com>
2014-01-05 15:56:24 +1100

    UserComponentSpec green

--- src/it/scala/forkpin/persist/UserComponentSpec.scala
+++ src/it/scala/forkpin/persist/UserComponentSpec.scala
@@ -11,19 +11,18 @@ class UserComponentSpec extends Specification { def is = s2"""
 
 """
 
+  val database = Database.forURL("jdbc:h2:mem:tests", driver = "org.h2.Driver")
   class UserRepo(override val profile: JdbcProfile) extends UserComponent with Profile
   val userRepo = new UserRepo(H2Driver)
-  val database = Database.forURL("jdbc:h2:mem:tests", driver = "org.h2.Driver")
 
   def saveAndLoad = {
     val expected = User("123", "Bob", new Timestamp(System.currentTimeMillis))
     val actual = database withSession { implicit s =>
+      userRepo.createUsersTable
       userRepo.insert(expected)
       userRepo.user("123")
     }
-    actual must_== expected
+    actual must_== Some(expected)
   }
 
 }
-
-// todo - is the difference around how the userRepo is constructed, compared to DAL?
